99f155cf8a7f94beb34784036cc97982f93fd877
Gondram <gondram@gmail.com>
2024-01-26 12:10:09 -0500

    almost done

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from sklearn.metrics import classification_report, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 import numpy as np
 import json
-import exam_functions as fn
+import functions as fn
 
 # load data
 with open('config/filepaths.json') as f:
@@ -28,8 +28,6 @@ X_train, y_train = load_Xy_data(fpath=FPATHS['data']['ml']['train'])
 
 X_test, y_test = load_Xy_data(fpath=FPATHS['data']['ml']['test'])
 
-
-
 def get_X_to_predict():
     X_to_predict = pd.DataFrame({'bedrooms': selected_beds,
                              'bathrooms': selected_baths, 
@@ -43,25 +41,20 @@ st.title('IMDB Rating Prediction')
 ## st.markdown("Data provided by the belt exam.")
                        
 st.subheader("Select model and enter text using the sidebar.\n Then check the box below to generate predictions.")
-                       
-st.sidebar.subheader('House Features')
-                       
-s_model = st.sidebar.selectbox('Model',['ML','NLP'])
+        
+s_model = st.sidebar.selectbox('Model',['ml','nlp'])
 
-selected_baths = st.sidebar.slider('Number of Bathrooms',min_value=0, max_value=8,value=2, step = 1)
-
-selected_lot = st.sidebar.number_input('Lot Square Footage',min_value = 500, 
-                                       max_value = 1075000, step = 500, value = 14500)
+s_text =st.sidebar.text_input('Insert Text')
 
 # Load model
 model = load_model_ml(fpath = FPATHS['models'][s_model])
 
 if st.checkbox("Generate"):
     
-    X_to_pred = get_X_to_predict()
-    new_pred = fn.get_prediction(linreg, X_to_pred)
+    X_to_pred = s_text
+    new_pred = fn.get_prediction(model, X_to_pred)
     
-    st.markdown(f"> #### Model Predicted Price = ${new_pred:,.0f}")
+    st.markdown(f"> #### {s_model} Model Predicted Rating = {new_pred:,.0f}")
     
 else:
     st.empty()
